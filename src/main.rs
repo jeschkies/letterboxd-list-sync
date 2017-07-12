caf0399606110b700e7746452e23b82c8317d152
Karsten Jeschkies <jeskar@web.de>
2017-07-12 21:25:51 +0200

    Crude file listing of folder.
    Next stpes
     * Use filter.
     * Pass folder with argument.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    match fs::read_dir("./") {
+        Ok(entries) => {
+            for entry in entries {
+                let e = entry.unwrap();
+                if let Ok(metadata) = e.metadata() {
+                    if metadata.is_file() {
+                        println!("{:?}", e.path());
+                    }
+                }
+            }
+        }
+        Err(e) => println!("Error!")
+    }
 }
