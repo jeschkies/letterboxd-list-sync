f59885aed869bd1aa19d3d044c5c01043339bbe2
Karsten Jeschkies <jeskar@web.de>
2017-07-13 10:09:25 +0200

    Filter files in iterator.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,27 @@
 use std::fs;
+use std::io;
+
+fn is_file(entry: &io::Result<fs::DirEntry>) -> bool {
+    match entry.as_ref() {
+      Err(_) => false,
+      Ok(ref e) => {
+        if let Ok(metadata) = e.metadata() {
+            metadata.is_file()
+        } else {
+            false
+        }
+      }
+    }
+}
 
 fn main() {
     match fs::read_dir("./") {
         Ok(entries) => {
-            for entry in entries {
+            for entry in entries.filter(is_file) {
                 let e = entry.unwrap();
-                if let Ok(metadata) = e.metadata() {
-                    if metadata.is_file() {
-                        println!("{:?}", e.path());
-                    }
-                }
+                println!("{:?}", e.path());
             }
         }
-        Err(e) => println!("Error!")
+        Err(_) => println!("Error!")
     }
 }
