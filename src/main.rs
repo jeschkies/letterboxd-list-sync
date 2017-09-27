3d9caf7df2e19dc5ccd570d585c2bb3eb80750f9
Karsten Jeschkies <jeskar@web.de>
2017-09-27 10:56:43 +0200

    Pass list id as parameter. (#7)
    * The list's name is still hardcoded.
    * Also format the code.

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ const USAGE: &'static str = "
 Letterboxid Sync. Synchronizes movies in a folder with a list on Letterboxd.
 
 Usage:
-    letterboxd-sync --pattern=<regex> <folder>
+    letterboxd-sync --pattern=<regex> <list-id> <folder>
 
 Options:
     --pattern=<regex>  The pattern used to extract the movie names.
@@ -30,36 +30,38 @@ Options:
 #[derive(Debug, Deserialize)]
 struct Args {
     flag_pattern: String,
-    arg_folder: String
+    arg_list_id: String,
+    arg_folder: String,
 }
 
 /// Returns true if entry is a file, false otherwise or on error.
 fn is_file(entry: &fs::DirEntry) -> bool {
-    entry.metadata().ok()
-        .map(|m| m.is_file())
-        .unwrap_or(false)
+    entry.metadata().ok().map(|m| m.is_file()).unwrap_or(false)
 }
 
 /// Used in to filter_map entries into files. Directories are sorted out.
 fn into_file(entry: io::Result<fs::DirEntry>) -> Option<fs::DirEntry> {
-    entry.ok().and_then(|e| {
-        if is_file(&e) {
-            Some(e)
-        } else {
-            None
-        }
-    })
+    entry.ok().and_then(
+        |e| if is_file(&e) { Some(e) } else { None },
+    )
 }
 
 /// List all files in dir.
-fn list_files(path: &str) -> Result<impl Iterator<Item = std::string::String>, Box<std::error::Error>> {
+fn list_files(
+    path: &str,
+) -> Result<impl Iterator<Item = std::string::String>, Box<std::error::Error>> {
     let dir = fs::read_dir(path)?;
-    let files = dir.filter_map(into_file).filter_map(|e| e.file_name().into_string().ok());
+    let files = dir.filter_map(into_file).filter_map(|e| {
+        e.file_name().into_string().ok()
+    });
     Ok(files)
 }
 
 /// Search movie on letterbox.
-fn search_movie(client: &letterboxd::Client, movie: std::string::String) -> Box<Future<Item = letterboxd::SearchResponse, Error = letterboxd::Error>> {
+fn search_movie(
+    client: &letterboxd::Client,
+    movie: std::string::String,
+) -> Box<Future<Item = letterboxd::SearchResponse, Error = letterboxd::Error>> {
     let request = letterboxd::SearchRequest {
         cursor: None,
         per_page: Some(1),
@@ -73,19 +75,24 @@ fn search_movie(client: &letterboxd::Client, movie: std::string::String) -> Box<
 
 /// Extract movie names from file names with given pattern.
 fn extract_movie(pattern: &Regex, file_name: &str) -> Option<String> {
-    pattern.captures(file_name)
+    pattern
+        .captures(file_name)
         .and_then(|matches| matches.get(1))
         .and_then(|m| String::from_str(m.as_str()).ok())
 }
 
 fn film_id_from_response(response: letterboxd::SearchResponse) -> Vec<String> {
-    response.items.into_iter().filter_map(|item| match item {
-        letterboxd::AbstractSearchItem::FilmSearchItem { film, .. } => Some(film.id),
-        _ => None
-    }).collect::<Vec<String>>()
+    response
+        .items
+        .into_iter()
+        .filter_map(|item| match item {
+            letterboxd::AbstractSearchItem::FilmSearchItem { film, .. } => Some(film.id),
+            _ => None,
+        })
+        .collect::<Vec<String>>()
 }
 
-fn sync_list(path: &str, pattern: &str) -> Result<(), Box<std::error::Error>> {
+fn sync_list(path: &str, pattern: &str, list_id: &str) -> Result<(), Box<std::error::Error>> {
     use tokio_core::reactor::Core;
 
     let mut core = Core::new().unwrap();
@@ -105,14 +112,18 @@ fn sync_list(path: &str, pattern: &str) -> Result<(), Box<std::error::Error>> {
     let movie_names = files.filter_map(|file_name| extract_movie(&re, file_name.as_str()));
     let requests = movie_names.map(|movie| search_movie(&client, movie));
     let film_ids = future::join_all(requests).map(|responses| -> Vec<_> {
-        responses.into_iter().flat_map(film_id_from_response).collect()
+        responses
+            .into_iter()
+            .flat_map(film_id_from_response)
+            .collect()
     });
 
-    let list_id = "1dcmE";
     let list_name = "to-watch";
     let result = film_ids.and_then(|ids| {
         let mut request = letterboxd::ListUpdateRequest::new(String::from(list_name));
-        request.entries = ids.into_iter().map(letterboxd::ListUpdateEntry::new).collect();
+        request.entries = ids.into_iter()
+            .map(letterboxd::ListUpdateEntry::new)
+            .collect();
         client.patch_list(list_id, &request, &token)
     });
 
@@ -125,5 +136,9 @@ fn main() {
         .and_then(|d| d.deserialize())
         .unwrap_or_else(|e| e.exit());
 
-    sync_list(args.arg_folder.as_str(), args.flag_pattern.as_str()).expect("Error!")
+    sync_list(
+        args.arg_folder.as_str(),
+        args.flag_pattern.as_str(),
+        args.arg_list_id.as_str(),
+    ).expect("Error!")
 }
